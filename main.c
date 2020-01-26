d5865b0de4324d905a93e0813f3ecea2925e648b
Your Name <you@example.com>
2020-01-26 20:33:04 +0100

    Change to 2 spaces indentation

--- main.c
+++ main.c
@@ -10,36 +10,36 @@ unsigned char receivedCharacter;
 unsigned char receivedFlag;
 
 int getTH1ForFrequency(int frequency) {
-    if(frequency == 19200) {
-        return 0xfd;
-    }
-    return -1;
+  if(frequency == 19200) {
+    return 0xfd;
+  }
+  return -1;
 }
 
 void init(int frequency)
 {
-    SCON = 0x50;
-    TMOD &= 0x0f;
-    TMOD |= 0x20;
+  SCON = 0x50;
+  TMOD &= 0x0f;
+  TMOD |= 0x20;
 
-    TCON = 0x40;
-    PCON = 0x80;
+  TCON = 0x40;
+  PCON = 0x80;
 
-    TH1 = getTH1ForFrequency(frequency); // th1 obliczamy z tej stronki tak zeby wyszlo 19200 jak w zadaniu
-    TR1 = 1;
-    TI = 0; // TI mowi o tym czy mozemy pisac czy czytac
-    EA = 1;
-    ES = 1;
-    IE = 0x90;
+  TH1 = getTH1ForFrequency(frequency); // th1 obliczamy z tej stronki tak zeby wyszlo 19200 jak w zadaniu
+  TR1 = 1;
+  TI = 0; // TI mowi o tym czy mozemy pisac czy czytac
+  EA = 1;
+  ES = 1;
+  IE = 0x90;
 
-    receivedFlag=0;
+  receivedFlag=0;
 }
 
 void put(unsigned char c)
 {
-    SBUF = c;
-    while(!TI){};
-    TI = 0;
+  SBUF = c;
+  while(!TI){};
+  TI = 0;
 }
 
 void put_BIN_U32(uint32_t w)
@@ -71,34 +71,34 @@ void put_BIN_U32(uint32_t w)
 
 void get_DEC_U32(unsigned char *x)
 {
-    while (receivedCharacter != ' '){
-        if(receivedFlag && (receivedCharacter <= '9' && receivedCharacter >= '0')) {
-            *x = *x * 10;
-            *x += receivedCharacter - '0';
-            receivedFlag = 0;
-        }
+  while (receivedCharacter != ' '){
+    if(receivedFlag && (receivedCharacter <= '9' && receivedCharacter >= '0')) {
+      *x = *x * 10;
+      *x += receivedCharacter - '0';
+      receivedFlag = 0;
     }
+  }
 }
 
 void catchInterrupt() interrupt 4
 {
-    if(RI)
-    {
-        receivedCharacter=SBUF;
-        RI=0;
-        receivedFlag=1;
-    }
+  if(RI)
+  {
+    receivedCharacter=SBUF;
+    RI=0;
+    receivedFlag=1;
+  }
 }
 
 void main(void)
 {
-    uint32_t z1;
-    uint32_t w;
-    init(19200);
-    for(;;) {
-        get_DEC_U32(&z1);
-        w = z1 - 3;
-        put_BIN_U32(w);
-        z1 = 0; // po wypisaniu musimy wyzerowac zmienna
-    }
+  uint32_t z1;
+  uint32_t w;
+  init(19200);
+  for(;;) {
+    get_DEC_U32(&z1);
+    w = z1 - 3;
+    put_BIN_U32(w);
+    z1 = 0; // po wypisaniu musimy wyzerowac zmienna
+  }
 }
